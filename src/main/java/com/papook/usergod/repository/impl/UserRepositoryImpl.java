9459b68d9ec16bc7f4794dcc00b55a70aedd00f5
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 13:14:38 +0400

    implemented the findAll method with paging and filtering

--- src/main/java/com/papook/usergod/repository/impl/UserRepositoryImpl.java
+++ src/main/java/com/papook/usergod/repository/impl/UserRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.papook.usergod.repository.impl;
 
+import static com.papook.usergod.config.ServerConfig.PAGE_SIZE;
+
 import java.util.Optional;
 
 import com.papook.usergod.model.User;
@@ -9,6 +11,10 @@ import com.papook.usergod.repository.UserRepository;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Root;
 import jakarta.transaction.Transactional;
 
 /**
@@ -70,10 +76,30 @@ public class UserRepositoryImpl implements UserRepository {
         return Optional.ofNullable(user);
     }
 
+    /**
+     * @param firstName the first name of the user. "%" to not filter by firstName
+     * @param lastName  the last name of the user. "%" to not filter by lastName
+     */
     @Override
     public Iterable<User> findAll(String firstName, String lastName, int page) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+        // Ensure that the page number is not less than 1
+        page = Math.max(1, page);
+        // Calculate the start position of the query
+        int startPosition = (page - 1) * PAGE_SIZE;
+
+        CriteriaBuilder cb = entityManager.getCriteriaBuilder();
+        CriteriaQuery<User> cq = cb.createQuery(User.class);
+        Root<User> root = cq.from(User.class);
+
+        cq.select(root).where(
+                cb.like(root.get("firstName"), firstName),
+                cb.like(root.get("lastName"), lastName));
+
+        TypedQuery<User> query = entityManager.createQuery(cq)
+                .setFirstResult(startPosition)
+                .setMaxResults(PAGE_SIZE);
+
+        return query.getResultList();
     }
 
     @Override
