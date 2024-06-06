bba8be465b377d643a07bd8b1f5bbcd5bf1d8257
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 20:09:51 +0400

    added the @Provider annotation to the WrongPasswordExceptionMapper

--- src/main/java/com/papook/usergod/repository/WrongPasswordExceptionMapper.java
+++ src/main/java/com/papook/usergod/repository/WrongPasswordExceptionMapper.java
@@ -4,7 +4,9 @@ import com.papook.usergod.model.ErrorMessage;
 
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
+@Provider
 public class WrongPasswordExceptionMapper implements ExceptionMapper<WrongPasswordException> {
 
     @Override
