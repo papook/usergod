4ac562538b5ee58af23e602cdb568dddb70f2ead
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 18:22:14 +0400

    added REGISTER_ENDPOINT constant to ServerConfig

--- src/main/java/com/papook/usergod/config/ServerConfig.java
+++ src/main/java/com/papook/usergod/config/ServerConfig.java
@@ -6,6 +6,8 @@ public class ServerConfig {
 
     public static final int PAGE_SIZE = 30;
 
-    public static final String USERS_ENDPOINT = "/users";
+    public static final String USERS_ENDPOINT = "users";
 
+    public static final String REGISTER_ENDPOINT = "register";
+    
 }
