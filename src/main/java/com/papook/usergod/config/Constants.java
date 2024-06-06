bbea63bc0c4f09828e3caffd1d5336f2664514d0
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 21:51:41 +0400

    new endpoints as constants added

--- src/main/java/com/papook/usergod/config/Constants.java
+++ src/main/java/com/papook/usergod/config/Constants.java
@@ -7,6 +7,8 @@ public class Constants {
     public static final int PAGE_SIZE = 30;
 
     public static final String USERS_ENDPOINT = "users";
+    public static final String USER_BY_ID_ENDPOINT = USERS_ENDPOINT + "/{id: \\d+}";
+    public static final String CHANGE_PASSWORD_ENDPOINT = USER_BY_ID_ENDPOINT + "/password";
 
     public static final String REGISTER_ENDPOINT = "register";
 
