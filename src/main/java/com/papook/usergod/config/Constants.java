b33892cc709ce59278c90a715f514c7878915249
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 20:26:03 +0400

    added a few more constants to Constants.java

--- src/main/java/com/papook/usergod/config/Constants.java
+++ src/main/java/com/papook/usergod/config/Constants.java
@@ -10,5 +10,13 @@ public class Constants {
 
     public static final String REGISTER_ENDPOINT = "register";
 
+    // Relation names
     public static final String GET_USER_COLLECTION_REL = "getUsersCollection";
+    public static final String GET_SINGLE_USER_REL = "getSingleUser";
+
+    public static final String UPDATE_USER_REL = "updateUser";
+    public static final String DELETE_USER_REL = "deleteUser";
+    public static final String CHANGE_PASSWORD_REL = "changePassword";
+
+    public static final String REGISTER_REL = "register";
 }
