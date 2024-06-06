76fe6a78af32e7b9f1822437b7a9f37240d5f52f
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 14:19:29 +0400

    added createUser and modifyUser methods to UserService

--- src/main/java/com/papook/usergod/service/UserService.java
+++ src/main/java/com/papook/usergod/service/UserService.java
@@ -1,8 +1,29 @@
 package com.papook.usergod.service;
 
-import jakarta.enterprise.context.ApplicationScoped;
+import com.papook.usergod.model.User;
+import com.papook.usergod.repository.UserRepository;
 
-@ApplicationScoped
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
 public class UserService {
-    // TODO: Implement service
+
+    @Inject
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        boolean existsByEmail = userRepository.existsByEmail(user.getEmail());
+
+        // If a user with the same email already exists, throw an exception
+        if (existsByEmail) {
+            throw new UserExistsException();
+        }
+
+        return userRepository.save(user);
+    }
+
+    public User modifyUser(Long id, User user) {
+        return userRepository.update(id, user);
+    }
 }
