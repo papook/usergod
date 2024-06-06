6c1c366739b5d2cf522b3a9170186fe77333c746
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 11:59:16 +0400

    added a short javadoc for IdMismatchException

--- src/main/java/com/papook/usergod/repository/IdMismatchException.java
+++ src/main/java/com/papook/usergod/repository/IdMismatchException.java
@@ -1,5 +1,11 @@
 package com.papook.usergod.repository;
 
+/**
+ * Exception thrown when the ID of the entity representation passed as a request
+ * body does not match the ID parameter of update the method.
+ * 
+ * @author papook
+ */
 public class IdMismatchException extends RuntimeException {
 
 }
