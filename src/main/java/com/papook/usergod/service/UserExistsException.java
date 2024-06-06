edc8cb1aed7e0ddcc33b589640bcc8ca0b8d6257
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-06 13:57:48 +0400

    added a new exception to throw when a user is trying to register with an email address that belongs to another user

---
+++ src/main/java/com/papook/usergod/service/UserExistsException.java
@@ -0,0 +1,11 @@
+package com.papook.usergod.service;
+
+/**
+ * Exception thrown when a user with the same email
+ * already exists in the database.
+ * 
+ * @author papook
+ */
+public class UserExistsException extends RuntimeException {
+
+}
