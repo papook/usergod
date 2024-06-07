1ff05218d9505e4b4e758fe7ca199ebc3e4d8b32
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-07 17:38:53 +0400

    update relation types to include the http method for better clarity

--- src/main/java/com/papook/usergod/config/Constants.java
+++ src/main/java/com/papook/usergod/config/Constants.java
@@ -16,9 +16,8 @@ public class Constants {
     public static final String GET_USER_COLLECTION_REL = "getUsersCollection";
     public static final String GET_SINGLE_USER_REL = "getUser";
 
-    public static final String UPDATE_USER_REL = "updateUser";
-    public static final String DELETE_USER_REL = "deleteUser";
-    public static final String CHANGE_PASSWORD_REL = "changePassword";
+    public static final String UPDATE_USER_REL = "putUpdateUser";
+    public static final String CHANGE_PASSWORD_REL = "putChangePassword";
 
-    public static final String REGISTER_REL = "register";
+    public static final String REGISTER_REL = "postRegisterUser";
 }
