492e9d811de1dccd1b95946743349615d2e540a2
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-07 16:17:23 +0400

    added UserNotFoundException and its mapper

---
+++ src/main/java/com/papook/usergod/service/UserNotFoundExceptionMapper.java
@@ -0,0 +1,25 @@
+package com.papook.usergod.service;
+
+import com.papook.usergod.model.ErrorMessage;
+
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+
+@Provider
+public class UserNotFoundExceptionMapper implements ExceptionMapper<UserNotFoundExistException> {
+
+    @Override
+    public Response toResponse(UserNotFoundExistException exception) {
+        int status = 404;
+
+        ErrorMessage errorMessage = new ErrorMessage(
+                status,
+                "The user does not exist. Check the ID and try again.");
+
+        return Response.status(status)
+                .entity(errorMessage)
+                .build();
+    }
+
+}
---
+++ src/main/java/com/papook/usergod/service/UserNotFoundExistException.java
@@ -0,0 +1,13 @@
+package com.papook.usergod.service;
+
+/**
+ * Exception thrown when the client tries to modify
+ * a user that does not exist.
+ * 
+ * @author papook
+ */
+public class UserNotFoundExistException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+}
