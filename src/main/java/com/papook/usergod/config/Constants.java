1a60633c0079a1f36534d31aa8550b3322adb8ee
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-07 17:37:00 +0400

    changed value of GET_SINGLE_USER_REL to "getUser"

--- src/main/java/com/papook/usergod/config/Constants.java
+++ src/main/java/com/papook/usergod/config/Constants.java
@@ -14,7 +14,7 @@ public class Constants {
 
     // Relation names
     public static final String GET_USER_COLLECTION_REL = "getUsersCollection";
-    public static final String GET_SINGLE_USER_REL = "getSingleUser";
+    public static final String GET_SINGLE_USER_REL = "getUser";
 
     public static final String UPDATE_USER_REL = "updateUser";
     public static final String DELETE_USER_REL = "deleteUser";
