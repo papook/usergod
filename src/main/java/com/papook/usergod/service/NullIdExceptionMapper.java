45d35401f6efb3eb8f3929e8597ce09d72b6f304
Sargsyan, Hayk <hayk.sargsyan@study.thws.de>
2024-06-07 16:10:59 +0400

    @Provider annotation added to NullIdExceptionMapper

--- src/main/java/com/papook/usergod/service/NullIdExceptionMapper.java
+++ src/main/java/com/papook/usergod/service/NullIdExceptionMapper.java
@@ -4,7 +4,9 @@ import com.papook.usergod.model.ErrorMessage;
 
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
 
+@Provider
 public class NullIdExceptionMapper implements ExceptionMapper<NullIdException> {
 
     @Override
